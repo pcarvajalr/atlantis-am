3da19ce4f35ccb1efda5d844d43708f548b40c52
Pedro Carvajal <galim_peiro96@hotmail.com>
2019-05-08 19:46:44 -0500

    actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component'
+
 import { ContactComponent } from './contact/contact.component';
 import { IndexComponent } from './index/index.component';
 import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes = [
-  {path: '', component: IndexComponent},
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
+  {path: 'index', component: IndexComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'gallery', component: GalleryComponent}
 ];
