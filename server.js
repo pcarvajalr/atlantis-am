17286ee1e968ed684b80f6a9fe6d09a129230b34
Pedro Carvajal <galim_peiro96@hotmail.com>
2019-05-08 20:15:09 -0500

    se actuzaliza json

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/atlantics'));
+app.use(express.static(__dirname + '/dist/Atlantics'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/atlantics/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/Atlantics/index.html'))
 });
 
 app.listen(process.env.PORT || 8080);
